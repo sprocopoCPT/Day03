f4a0c7d14ac905d16296494bf0e04a7fe767d1eb
sprocopoCPT <67952406+sprocopoCPT@users.noreply.github.com>
2020-07-15 13:48:25 +0200

    Update ft_ft.c

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -1,44 +1,5 @@
 
-#include <unistd.h>
-
-void ft_putchar(char c)
-{
-write (1, &c, 1);
-}
-
-void ft_putnbr(int nb)
-{
-  long i;
-  i = nb;
-
-  if (i < 0)
-  {
-    ft_putchar('-');
-    i = i * (-1);
-  }
-  if (i > 9)
-  {
-    ft_putnbr(i / 10);
-    ft_putnbr(i % 10);
-  }
-  else if (i < 10)
-  {
-    ft_putchar(i + '0');
-  }
-}
-
-
 void ft_ft(int *nbr)
 {
 	*nbr = 42;
-}
-
-int main(void)
-{
-int	*nbr;
-*nbr = 42;
-
-
-	ft_putnbr(*nbr);
-	return 0;
 }
